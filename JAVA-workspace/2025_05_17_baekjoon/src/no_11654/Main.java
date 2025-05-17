2c3e4982578b2b8cdd8be21b9423c041026f8d4f
kihoonkwon <jhgfjhgfjhgf96@gmail.com>
2025-05-17 20:54:22 +0900

    2025_05_17

---
+++ JAVA-workspace/2025_05_17_baekjoon/src/no_11654/Main.java
@@ -0,0 +1,13 @@
+package no_11654;
+
+public class Main {
+	public static void main(String[] args) {
+		int ascii;
+		try {
+			ascii = System.in.read();
+			System.out.println(ascii);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+}
