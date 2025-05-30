10b344821ce60abbc5f1e66c15b1a26fb041f38a
kihoonkwon <jhgfjhgfjhgf96@gmail.com>
2025-05-30 23:10:37 +0900

    2025_05_30

---
+++ JAVA-workspace/2025_05_30_baekjoon/src/no_15894/Main.java
@@ -0,0 +1,16 @@
+package no_15894;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		long perimeter = 4 * n;
+		
+		System.out.println(perimeter);
+	}
+}
