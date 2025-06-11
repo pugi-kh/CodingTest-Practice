c44f4c54454a677715b4503f99604074f08b680d
kihoonkwon <jhgfjhgfjhgf96@gmail.com>
2025-06-11 20:51:41 +0900

    2025_06_11

--- git-push.sh
+++ git-push.sh
@@ -6,26 +6,16 @@ echo "git push run? [y/n]"
 
 read answer
 
-<<<<<<< HEAD
-if [ "$answer" = "y" ]; then
+if [[ "$answer" == "y" ]]; then
   git add .
   sleep 1
   git add .
   git status
-=======
-git add .
-sleep 1
-git add .
-git status
-git commit -m "$date"
-git push origin main
->>>>>>> 89977c54aa2ef7a8f57997dca8ab806e86053bb0
 
   date=$(date +%Y_%m_%d)
-  
   git commit -m "$date"
   git push origin main
-
+  
   git log -n 10 --oneline
 else
   echo "git push canceled"
